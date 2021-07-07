eacedb40f4ca3702537358a1b4738bf92364754d
Robert Mende <robert.mende1996@web.de>
2021-07-07 14:37:41 +0200

    Add Model

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrüfungsProjekt
+{
+    class Class1
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+            Console.ReadKey(); 
+
+        }
+    }
+}
