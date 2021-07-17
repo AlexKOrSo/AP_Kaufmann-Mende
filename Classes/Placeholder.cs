2c30ba135be5469c8544cb30979d883ca4d50a5c
robert.mende@chemie.tu-freiberg.de <robert.mende@chemie.tu-freiebrg.de>
2021-07-17 19:01:53 +0200

    Add PlaceHolder to test Solution

---
+++ Classes/Placeholder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace Classes
+{
+    public static class PlaceHolder
+    {
+        public static void PrintPlaceHolder()
+        {
+            Console.WriteLine("Succesfully called the PlaceHolder"); 
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Classes; 
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            PlaceHolder.PrintPlaceHolder(); //Check for succesful reference
+        }
+    }
+}
