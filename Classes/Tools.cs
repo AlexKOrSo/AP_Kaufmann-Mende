55f1bb8153e0eb1854bb8a60d5b3750f3306de3f
AlexKOrSo <alexander.k347@outlook.de>
2021-07-18 15:10:05 +0200

    Added Toolsclass and example to main

---
+++ Classes/Tools.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Tools
+{
+    public static class ConsoleTools
+    {
+        public static bool YesNoInput(string question)
+        {
+            ConsoleKeyInfo key;
+            do
+            {
+                Console.Write(question + @" [y/n]");
+                key = Console.ReadKey(false);
+                Console.WriteLine();
+            } while (key.Key!= ConsoleKey.Y && key.Key!=ConsoleKey.N);
+
+            return (key.Key == ConsoleKey.Y);
+        }
+    }
+}
